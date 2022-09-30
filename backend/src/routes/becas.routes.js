9ceee283666c11afebd48ca05d06dafe38c62db2
Francisco Moretti <francisco.moretti@alumnos.uv.cl>
2022-09-29 22:13:13 -0300

    endpoint agregado

--- backend/src/routes/becas.routes.js
+++ backend/src/routes/becas.routes.js
@@ -7,24 +7,25 @@ import {
 
 const router = Router()
 
+//endpoint alumnos
 router.get('/alumnos', verAlumnos);
-
 router.get('/alumnos/:id', verAlumnoId);
-
 router.post('/alumnos', agregarAlumno);
-
 router.delete('/alumnos/:id', borrarAlumno);
-
 router.put('/alumnos/:id', editarAlumno);
 
+//endpoint casinos
 router.get('/casinos', verCasinos);
-
 router.get('/casinos/:id', verCasinoId);
-
 router.post('/casinos', agregarCasino);
-
 router.delete('/casinos/:id', borrarCasino);
-
 router.put('/casinos/:id', editarCasino);
+
+//endpoint canjes
+router.get('/canjes', verCasinos);
+router.get('/canjes/:id', verCanjeId);
+router.put('/canjes/:id', Canje);
+/* router.post('/canjes', agregarCanje); 
+router.delete('/canjes/:id', borrarCanje); */
 
 export default router
