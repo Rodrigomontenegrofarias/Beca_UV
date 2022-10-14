a4c72538819cc25c61a34335bf643571d251fd9c
Francisco Moretti <francisco.moretti@alumnos.uv.cl>
2022-10-14 15:46:49 -0300

    cerrar sesion button added

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  loading = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  isLoading(){
+    this.loading = true;
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+      this.loading = false
+  },2000);
   }
 
 }
